17723d5ac128016a878ce527db4711965e83a56c
Juan Pablo Orrego Contador <jporregoc@gmail.com>
2022-07-27 15:11:12 -0400

    Initial interface design

--- src/features/board/BoardList.tsx
+++ src/features/board/BoardList.tsx
@@ -1,5 +1,5 @@
 import React, { useEffect } from "react";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 import { useAppSelector, useAppDispatch } from "../../app/hooks";
 import {
   selectAllBoards,
@@ -7,10 +7,15 @@ import {
   selectBoardsStatus,
 } from "../../features/board/boardsSlice";
 
+import styles from "./BoardList.module.css";
+import { MdOutlineDashboardCustomize } from "react-icons/md";
+
 const BoardList = () => {
   const dispatch = useAppDispatch();
   const boards = useAppSelector(selectAllBoards);
   const boardStatus = useAppSelector(selectBoardsStatus);
+
+  const navigate = useNavigate();
 
   useEffect(() => {
     if (boardStatus === "idle") {
@@ -19,12 +24,26 @@ const BoardList = () => {
   }, [boardStatus]);
 
   const renderedBoards = boards.map((board) => (
-    <div key={board.id}>
-      <Link to={`/boards/${board.id}`}>{board.name}</Link>
+    <div
+      key={board.id}
+      className={styles.boardCard}
+      onClick={() => navigate(`/boards/${board.id}`)}
+    >
+      <div>{board.name}</div>
+      <Link to={`/boards/${board.id}`}></Link>
     </div>
   ));
 
-  return <div>{renderedBoards}</div>;
+  return (
+    <div className={styles.boardList}>
+      <div className={styles.boardListTitle}>
+        {" "}
+        <MdOutlineDashboardCustomize></MdOutlineDashboardCustomize>Boards
+      </div>
+
+      <div className={styles.boardCards}>{renderedBoards}</div>
+    </div>
+  );
 };
 
 export default BoardList;
