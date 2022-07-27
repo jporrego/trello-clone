80228e2467d9f6c312ee982881d037d4bbd57a65
Juan Pablo Orrego Contador <jporregoc@gmail.com>
2022-07-27 15:19:18 -0400

    Added trello icon

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -1,8 +1,14 @@
 import React from "react";
 import styles from "./Navbar.module.css";
+import { FaTrello } from "react-icons/fa";
 
 const Navbar = () => {
-  return <div className={styles.navbar}>Navbar</div>;
+  return (
+    <div className={styles.navbar}>
+      <FaTrello></FaTrello>
+      Trello
+    </div>
+  );
 };
 
 export default Navbar;
