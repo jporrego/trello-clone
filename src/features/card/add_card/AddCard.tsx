97f097190f190f4fe20fe85db11af211a03b5643
Juan Pablo Orrego Contador <jporregoc@gmail.com>
2022-07-28 15:57:36 -0400

    AddCard document event listener

--- src/features/card/add_card/AddCard.tsx
+++ src/features/card/add_card/AddCard.tsx
@@ -1,19 +1,35 @@
-import React, { useState } from "react";
+import React, { useState, useEffect, useRef } from "react";
 import { AiOutlinePlus } from "react-icons/ai";
 import styles from "../Card.module.css";
 
 const AddCard = () => {
   const [isFormVisible, setIsFormVisible] = useState(false);
+  const ref = useRef(null);
+
+  useEffect(() => {
+    if (isFormVisible) {
+      document.addEventListener("click", (e) => handleClickOutside(e), true);
+    } else {
+      document.removeEventListener("click", (e) => handleClickOutside(e), true);
+    }
+  }, [isFormVisible]);
+
+  const handleClickOutside = (e: MouseEvent) => {
+    if (e.target instanceof Element) {
+      console.log(e.target.tagName);
+    }
+  };
 
   return (
     <div className={`${styles.addCard} transition50ms`}>
       {isFormVisible ? (
         <form action="">
           <textarea name="" id=""></textarea>
+          <div className={styles.addCard__btn}>Add Card</div>
         </form>
       ) : (
         <div
-          className={styles.addCard__btn}
+          className={styles.showForm__btn}
           onClick={() => setIsFormVisible(true)}
         >
           <AiOutlinePlus></AiOutlinePlus> Add a card
