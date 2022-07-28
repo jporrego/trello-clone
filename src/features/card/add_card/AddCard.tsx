03c68a00527c3203b25dc7038e9ef9249a18ba2e
Juan Pablo Orrego Contador <jporregoc@gmail.com>
2022-07-28 17:51:39 -0400

    Fixed functionallity of hiding form

--- src/features/card/add_card/AddCard.tsx
+++ src/features/card/add_card/AddCard.tsx
@@ -1,25 +1,38 @@
-import React, { useState, useEffect, useRef } from "react";
+import React, { useState, useEffect } from "react";
+import axios from "axios";
 import { AiOutlinePlus } from "react-icons/ai";
 import styles from "../Card.module.css";
 
 const AddCard = () => {
   const [isFormVisible, setIsFormVisible] = useState(false);
   const [cardName, setCardName] = useState<string>();
-  const ref = useRef(null);
 
   useEffect(() => {
+    const handleClickOutside = (e: MouseEvent) => {
+      console.log(1);
+      if (e.target instanceof Element) {
+        if (e.target.id !== "card-name" && e.target.id !== "addCard__btn") {
+          setIsFormVisible(false);
+        }
+      }
+    };
+
     if (isFormVisible) {
-      document.addEventListener("click", (e) => handleClickOutside(e), true);
-    } else {
-      document.removeEventListener("click", (e) => handleClickOutside(e), true);
+      document.addEventListener("click", handleClickOutside, true);
     }
+
+    return () => {
+      document.removeEventListener("click", handleClickOutside, true);
+    };
   }, [isFormVisible]);
 
-  const handleClickOutside = (e: MouseEvent) => {
-    if (e.target instanceof Element) {
-      if (e.target.tagName !== "TEXTAREA") {
-        setIsFormVisible(false);
-      }
+  const handleAddCard = async () => {
+    try {
+      const url = process.env.REACT_APP_API_URL + `api/cards/${1}`;
+      const response = await axios.post(url);
+      console.log(response);
+    } catch (error) {
+      console.log(error);
     }
   };
 
@@ -34,7 +47,13 @@ const AddCard = () => {
             onChange={(e) => setCardName(e.target.value)}
             placeholder="Enter a title for this card..."
           ></textarea>
-          <div className={styles.addCard__btn}>Add Card</div>
+          <div
+            id="addCard__btn"
+            className={styles.addCard__btn}
+            onClick={handleAddCard}
+          >
+            Add Card
+          </div>
         </form>
       ) : (
         <div
