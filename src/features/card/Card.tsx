e9bdf70979f1a85406173e8b3f1024112ba91527
Juan Pablo Orrego Contador <jporregoc@gmail.com>
2022-07-29 16:37:12 -0400

    Delete card feature

--- src/features/card/Card.tsx
+++ src/features/card/Card.tsx
@@ -1,13 +1,34 @@
-import React from "react";
+import React, { useState } from "react";
 import { Card as CardType } from "../../types";
 import styles from "./Card.module.css";
+import { TiDeleteOutline } from "react-icons/ti";
 
 interface CardProps {
   card: CardType;
 }
 
 const Card: React.FC<CardProps> = ({ card }) => {
-  return <div className={styles.card}>{card.name}</div>;
+  const [isCardHovered, setIsCardHovered] = useState(true);
+  const handleDeleteCard = () => {
+    alert(1);
+  };
+  return (
+    <div
+      className={styles.card}
+      onMouseEnter={() => setIsCardHovered(true)}
+      onMouseLeave={() => setIsCardHovered(false)}
+    >
+      {card.name}
+      <div className={styles.btnDeleteCard}>
+        {isCardHovered && (
+          <TiDeleteOutline
+            className={styles.btnDeleteCard}
+            onClick={handleDeleteCard}
+          ></TiDeleteOutline>
+        )}
+      </div>
+    </div>
+  );
 };
 
 export default Card;
--- src/index.tsx
+++ src/index.tsx
@@ -2,11 +2,14 @@ import React from "react";
 import { createRoot } from "react-dom/client";
 import { Provider } from "react-redux";
 import { store } from "./app/store";
+import { fetchBoards } from "./features/board/boardsSlice";
 import App from "./App";
 import "./index.css";
 
 const container = document.getElementById("root")!;
 const root = createRoot(container);
+
+store.dispatch(fetchBoards());
 
 root.render(
   <React.StrictMode>
