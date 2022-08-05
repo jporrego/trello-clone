ca1968bc185a7c320714167902916154ef756ebd
Juan Pablo Orrego Contador <jporregoc@gmail.com>
2022-08-05 17:47:30 -0400

    Description btn

--- src/components/card_modal/description/Description.tsx
+++ src/components/card_modal/description/Description.tsx
@@ -1,9 +1,12 @@
-import React from "react";
+import React, { useState } from "react";
 import styles from "./Description.module.css";
 import cardModalStyles from "../../../features/card/card_modal/CardModal.module.css";
 import { MdOutlineDescription } from "react-icons/md";
 
 const Description = () => {
+  const [description, setDescription] = useState<string>("");
+  const [showForm, setShowForm] = useState<boolean>(false);
+
   return (
     <div>
       <div className={styles.description}>
@@ -11,13 +14,22 @@ const Description = () => {
           <MdOutlineDescription></MdOutlineDescription>
         </div>
         <div className={styles.description_title}>Description</div>
-        <form>
-          <textarea
-            className={styles.description_textArea}
-            placeholder="Add a more detailed description..."
-            maxLength={850}
-          ></textarea>
-        </form>
+        {showForm ? (
+          <form>
+            <textarea
+              className={styles.description_textArea}
+              placeholder="Add a more detailed description..."
+              maxLength={850}
+            ></textarea>
+          </form>
+        ) : (
+          <div
+            className={styles.btnShowTextarea}
+            onClick={() => setShowForm(true)}
+          >
+            Add a more detailed description...
+          </div>
+        )}
       </div>
     </div>
   );
