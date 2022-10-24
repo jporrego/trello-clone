2a3c33560e1addbfea5156aa7fbf02ecb2f7d145
Juan Pablo Orrego Contador <jporregoc@gmail.com>
2022-10-24 18:56:33 -0300

    handleAddBoard function

--- src/features/board/add_board/AddBoardForm.tsx
+++ src/features/board/add_board/AddBoardForm.tsx
@@ -1,21 +1,44 @@
 import React, { useState } from "react";
 import { BackgroundSelector } from "./BackgroundSelector";
+import axios from "axios";
+import { useAppSelector } from "../../../app/hooks";
+import { selectUser } from "../../../features/user/UserSlice";
 import styles from "./AddBoard.module.css";
 
 const AddBoardForm = () => {
+  const user = useAppSelector(selectUser);
   const [boardData, setBoardData] = useState({
     title: "",
-    backgroundImg: "",
-    backgroundColor: "",
+    bgImg: "",
+    bgColor: "",
   });
 
   const onChangeBackground = (img: string, color: string) => {
     if (img.length > 0) {
-      setBoardData({ ...boardData, backgroundImg: img, backgroundColor: "" });
+      setBoardData({ ...boardData, bgImg: img, bgColor: "" });
     } else {
-      setBoardData({ ...boardData, backgroundImg: "", backgroundColor: color });
+      setBoardData({ ...boardData, bgImg: "", bgColor: color });
     }
   };
+
+  const handleAddBoard = async () => {
+    if (boardData.title.trim().length > 0) {
+      try {
+        const url = process.env.REACT_APP_API_URL + `api/boards/`;
+        const data = {
+          userId: user.id,
+          title: boardData.title,
+          bgImg: boardData.bgImg,
+          bgColor: boardData.bgColor,
+        };
+        const res = await axios.post(url, data);
+        console.log(res);
+      } catch (error) {
+        console.log(error);
+      }
+    }
+  };
+
   return (
     <div className={styles.add_board_form}>
       <BackgroundSelector
@@ -33,8 +56,9 @@ const AddBoardForm = () => {
             setBoardData({ ...boardData, title: e.target.value })
           }
         />
+        {!boardData.title && <p>👋 Board title is required</p>}
       </label>
-      <button onClick={() => console.log(boardData)}>Create</button>
+      <button onClick={() => handleAddBoard()}>Create</button>
     </div>
   );
 };
